1aac0f6bdf2b27e81b61717c4fc81358004fc1f2
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-20 15:37:53 -0800

    pass first test
    Co-authored-by: Logan Roth <diamondintheroth@gmail.com>
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
-Describe: `pigLatin()`
-Test: "It will add 'way' to the end of words that begin with a vowel"
-Expect(pigLatin("a")).toEqual("away");
+// Describe: `pigLatin()`
+// Test: "It will add 'way' to the end of words that begin with a vowel"
+// Expect(pigLatin("a")).toEqual("away");
 
-Test: "It will move all of the first consecutive consonants to the end, and add 'ay'."
-Expect(pigLatin("tie").toEqual("ietay"));
+// Test: "It will move all of the first consecutive consonants to the end, and add 'ay'."
+// Expect(pigLatin("tie").toEqual("ietay"));
 
-Test: "It will move 'qu' to the end along with consonants, and add 'ay'."
-Expect(pigLatin("squeal").toEqual("ealsquay"));
+// Test: "It will move 'qu' to the end along with consonants, and add 'ay'."
+// Expect(pigLatin("squeal").toEqual("ealsquay"));
 
-Test: "It will treat 'y' as a consonant, and move it to the end and add 'ay'"
-Expect(pigLatin("yellow").toEqual("ellowyay"));
+// Test: "It will treat 'y' as a consonant, and move it to the end and add 'ay'"
+// Expect(pigLatin("yellow").toEqual("ellowyay"));
 
+
+
+function pigLatin(word) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const letterToCheck = word[0];
+  if (vowels.includes(letterToCheck)) {
+    return word + "way"
+  } else {
+    return word
+  }
+}
+
+console.log((pigLatin("stupid")))
