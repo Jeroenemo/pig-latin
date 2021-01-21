143f052ebfca0111b781de2de35003b665dfdca7
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-20 16:16:03 -0800

    pass second test
    Co-authored-by: Logan Roth <diamondintheroth@gmail.com>
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,16 @@ function pigLatin(word) {
   if (vowels.includes(word[0])) {
     return word + "way"
   } else {
+    for (let index = 0; index < word.length; index +=1){
+      if (vowels.includes(word[index])){
+      const firstConsonant = word.slice(0,index);
+      const leftOvers = word.slice(index, word.length-1)
+      index = word.length;
+      return leftOvers + firstConsonant + "ay";
+      }
+    }
     return word
   }
 }
 
-console.log(pigLatin("away"))
+console.log(pigLatin("school"))
